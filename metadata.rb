2435ae8f83c69dc0ff65c61197b81a16022c5a6a
Brandon Turner <bt@brandonturner.net>
2015-04-05 23:59:31 -0500

    Ensure cookbook works with latest nginx cookbook

--- metadata.rb
+++ metadata.rb
@@ -4,13 +4,13 @@ maintainer_email 'ryan_hass@rapid7.com'
 license          'Apache 2.0'
 description      'Installs/Configures sonarqube'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.10'
+version          '0.2.0'
 
 supports 'ubuntu'
 
-depends 'java', '>= 1.22.0'
 depends 'apt'
 depends 'database'
-depends 'postgresql'
-depends 'nginx', '= 2.4.2'
+depends 'java', '>= 1.22.0'
+depends 'nginx', '>= 2.4.2'
 depends 'openssl'
+depends 'postgresql'
